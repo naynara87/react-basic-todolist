9a830aa1fd465186600f51f32594a97d2aefeb7e
naynara87 <naynara87@gmail.com>
2021-06-22 16:23:55 +0900

    2차수정

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import useReduxState from "../hooks/useReduxState"
+
+export default function TodoList() {
+
+    const state = useReduxState ();
+    
+    return(
+       <ul>
+           {state.todos.map((todo) => {
+               return <li>{todo.text}</li>;
+           })}
+        </ul>
+    ) 
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+// import { addTodo, completeTodo, showComplete } from './redux/actions';
+import ReduxContext from './contexts/ReduxContext';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// console.log(store);
+// store.dispatch(addTodo('할일'));
+// store.dispatch(completeTodo(0));
+// store.dispatch(showComplete());
+
+
+ReactDOM.render( 
+  <React.StrictMode >
+    <ReduxContext.Provider value={store}>
+      <App />  
+    </ReduxContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
